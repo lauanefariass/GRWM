c942f467c421acf0a1119da88388e8f8c68e08a7
lauanefariass <lauane.fcarneiro@gmail.com>
2024-09-24 17:28:39 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,30 @@ const Grwm = mongoose.model("GRWM", {
 
 app.use(express.json());
 
+app.post("/", async (req, res) => {
+  const newGRWM = new Grwm({
+    brand: req.body.brand,
+    price: req.body.price,
+    description: req.body.description,
+    image_url: req.body.image_url,
+  });
+  await newGRWM.save();
+  return res.send("Successfully created");
+});
 
+app.put("/:id", async (req, res) => {
+  const updatedGRWM = await Grwm.findByIdAndUpdate(
+    req.params.id,
+    {
+      brand: req.body.brand,
+      price: req.body.price,
+      description: req.body.description,
+      image: req.body.image,
+    },
+    { new: true }
+  );
+  return res.send("Successfully updated");
+});
 
 
 mongoose
